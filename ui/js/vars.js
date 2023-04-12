d3390a4b623984868a55e0a2c0fac93fdfe4d739
Oliver Doerr <oldorr@microsoft.com>
2023-04-12 14:42:03 +0200

    updated Target URL for the API

--- ui/js/vars.js
+++ ui/js/vars.js
@@ -1,3 +1,3 @@
 ﻿// Put the urls to your local api and your remote API here
 var localUrl = "http://localhost:7071/api/todoitem";
-var remoteUrl = "https://<YOUR_FUNCTIONS_URL>/api/todoitem";
+var remoteUrl = "https://delightful-beach-0a3f64d03.2.azurestaticapps.net/api/todoitem";
